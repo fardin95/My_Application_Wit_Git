aa3787b1fd8fd316eb4fca1b34c1f8ba8b264f3d
fardin95 <fardin95@gmail.com>
2016-05-01 17:05:13 +0600

    add a activity

--- app/src/main/java/com/example/git/kazimdsaidul/myapplicationwithgit/AActivity.java
+++ app/src/main/java/com/example/git/kazimdsaidul/myapplicationwithgit/AActivity.java
@@ -3,13 +3,11 @@ package com.example.git.kazimdsaidul.myapplicationwithgit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AActivity extends AppCompatActivity {
 
-    private int add = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_a);
     }
 }
